a0152a0ff66141d0b991e2d8eb930924908269ac
Anastasia Mitrevski <rushin.doll@gmail.com>
2010-10-26 14:15:19 -0400

    3.0.0 - Definitions of necessary information for multiple-site support.  Cribbed from the LJlogin project (thanks!)

---
+++ lj_site_definitions.js
@@ -0,0 +1,53 @@
+// Super special thanks to the LJlogin project from w hich all this information was gleaned.  There were changes made to make this work in Chrome, but hours of research were saved!
+
+var LJlogin_sites = [
+	{
+		name: 'LiveJournal',
+		domain: '.livejournal.com',
+		cookieurl: 'http://www.livejournal.com/',
+		cookiename: 'ljmastersession',
+		interfaceurl: 'http://www.livejournal.com/interface/flat',
+	},
+	{
+		name: 'InsaneJournal',
+		domain: '.insanejournal.com',
+		cookieurl: 'http://www.insanejournal.com/',
+		cookiename: 'ljmastersession',
+		interfaceurl: 'http://www.insanejournal.com/interface/flat',
+	},
+	{
+		name: 'JournalFen',
+		domain: '.journalfen.net',
+		cookieurl: 'http://www.journalfen.net/',
+		cookiename: 'ljsession',
+		interfaceurl: 'http://www.journalfen.net/interface/flat',
+	},
+	{
+		name: 'DeadJournal',
+		domain: '.deadjournal.com',
+		cookieurl: 'http://www.deadjournal.com/',
+		cookiename: 'ljmastersession',
+		interfaceurl: 'http://www.deadjournal.com/interface/flat',
+	},
+	{
+		name: 'Scribbld',
+		domain: '.scribbld.net',
+		cookieurl: 'http://www.scribbld.net/',
+		cookiename: 'ljmastersession',
+		interfaceurl: 'http://www.scribbld.net/interface/flat',
+	},
+	{
+		name: 'Dreamwidth',
+		domain: '.dreamwidth.org',
+		cookieurl: 'http://www.dreamwidth.org/',
+		cookiename: 'ljmastersession',
+		interfaceurl: 'http://www.dreamwidth.org/interface/flat',
+	},
+	{
+		name: 'Inksome',
+		domain: '.inksome.com',
+		cookieurl: 'http://www.inksome.com/',
+		cookiename: 'ljmastersession',
+		interfaceurl: 'http://www.inksome.com/interface/flat',
+	}
+];
