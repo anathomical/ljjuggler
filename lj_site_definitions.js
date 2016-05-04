99529be76886a210091b2d29b38e10b9265fe641
kalinda99 <kalinda@teksavvy.com>
2016-05-04 11:58:25 -0400

    Removed dead websites
    Also moved DW to the top of the list because most people use it and because I could.

--- lj_site_definitions.js
+++ lj_site_definitions.js
@@ -1,6 +1,13 @@
-// Super special thanks to the LJlogin project from w hich all this information was gleaned.  There were changes made to make this work in Chrome, but hours of research were saved!
+// Super special thanks to the LJlogin project from which all this information was gleaned.  There were changes made to make this work in Chrome, but hours of research were saved!
 
 var LJlogin_sites = [
+	{
+		name: 'Dreamwidth',
+		domain: '.dreamwidth.org',
+		cookieurl: 'http://www.dreamwidth.org/',
+		cookiename: 'ljmastersession',
+		interfaceurl: 'http://www.dreamwidth.org/interface/flat',
+	},
 	{
 		name: 'LiveJournal',
 		domain: '.livejournal.com',
@@ -14,47 +21,5 @@ var LJlogin_sites = [
 		cookieurl: 'http://www.insanejournal.com/',
 		cookiename: 'ljmastersession',
 		interfaceurl: 'http://www.insanejournal.com/interface/flat',
-	},
-	{
-		name: 'JournalFen',
-		domain: '.journalfen.net',
-		cookieurl: 'http://www.journalfen.net/',
-		cookiename: 'ljsession',
-		interfaceurl: 'http://www.journalfen.net/interface/flat',
-	},
-	{
-		name: 'DeadJournal',
-		domain: '.deadjournal.com',
-		cookieurl: 'http://www.deadjournal.com/',
-		cookiename: 'ljmastersession',
-		interfaceurl: 'http://www.deadjournal.com/interface/flat',
-	},
-	{
-		name: 'Scribbld',
-		domain: '.scribbld.com',
-		cookieurl: 'http://www.scribbld.com/',
-		cookiename: 'ljmastersession',
-		interfaceurl: 'http://www.scribbld.com/interface/flat',
-	},
-	{
-		name: 'Scribbld.us',
-		domain: '.scribbld.us',
-		cookieurl: 'http://www.scribbld.us/',
-		cookiename: 'ljmastersession',
-		interfaceurl: 'http://www.scribbld.us/interface/flat',
-	},
-	{
-		name: 'Dreamwidth',
-		domain: '.dreamwidth.org',
-		cookieurl: 'http://www.dreamwidth.org/',
-		cookiename: 'ljmastersession',
-		interfaceurl: 'http://www.dreamwidth.org/interface/flat',
-	},
-	{
-		name: 'Inksome',
-		domain: '.inksome.com',
-		cookieurl: 'http://www.inksome.com/',
-		cookiename: 'ljmastersession',
-		interfaceurl: 'http://www.inksome.com/interface/flat',
 	}
 ];
